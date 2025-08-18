8f4b79469b8de9aee25323cd4beab3970e3da393
Maksym <112901851+Maksym82@users.noreply.github.com>
2025-08-18 20:04:56 +0300

    Update styles

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,20 @@
-document.querySelectorAll(".nav-item").forEach((item) => {
-  item.addEventListener("click", () => {
-    document
-      .querySelectorAll(".nav-item")
-      .forEach((i) => i.classList.remove("active"));
-    item.classList.add("active");
-    // Можно добавить логику навигации или отображения контента
-    console.log("Выбран:", item.dataset.label);
+// document.querySelectorAll(".nav-item").forEach((item) => {
+//   item.addEventListener("click", () => {
+//     document
+//       .querySelectorAll(".nav-item")
+//       .forEach((i) => i.classList.remove("active"));
+//     item.classList.add("active");
+//     // Можно добавить логику навигации или отображения контента
+//     console.log("Выбран:", item.dataset.label);
+//   });
+// });
+
+const navItems = document.querySelectorAll('.nav-item');
+
+navItems.forEach(item => {
+  item.addEventListener('click', () => {
+    navItems.forEach(btn => btn.classList.remove('active'));
+    item.classList.add('active');
   });
 });
+
